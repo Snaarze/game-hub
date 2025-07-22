4ad9534322760ade776cd7d74aaf73ba2dca230d
Snaarze <panlasiguijeremyglenn49@gmail.com>
2025-07-22 08:56:52 +0800

    fixed genre name is not showing

--- src/components/CardList/GameTitle.tsx
+++ src/components/CardList/GameTitle.tsx
@@ -8,7 +8,7 @@ interface Props {
 }
 
 const GameTitle = ({ platform, genre }: Props) => {
-  const heading = `${platform || ""} ${genre || ""} Games`;
+  const heading = `${platform?.slug || ""} ${genre?.name || ""} Games`;
   return <h1 className="font-bold text-3xl mb-2">{heading}</h1>;
 };
 
